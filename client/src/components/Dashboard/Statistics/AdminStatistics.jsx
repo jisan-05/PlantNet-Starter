06fbdd7c7e874644a93240cca1f4ada01b76e2fa
Tanvir <zishantanvir05@gmail.com>
2025-06-01 19:28:36 +0600

    use aggregate for get chart data

--- client/src/components/Dashboard/Statistics/AdminStatistics.jsx
+++ client/src/components/Dashboard/Statistics/AdminStatistics.jsx
@@ -4,6 +4,7 @@ import { BsFillCartPlusFill, BsFillHouseDoorFill } from 'react-icons/bs'
 import { useQuery } from '@tanstack/react-query'
 import useAxiosSecure from '../../../hooks/useAxiosSecure'
 import LoadingSpinner from '../../Shared/LoadingSpinner'
+import Chart from '../../../pages/Dashboard/Charts/Chart'
 
 const AdminStatistics = () => {
   const axiosSecure = useAxiosSecure()
@@ -16,7 +17,7 @@ const AdminStatistics = () => {
     }
   })
   console.log(statData)
-  const {totalUser,totalPlants,totalOrder,totalRevenue} = statData || {};
+  const {totalUser,totalPlants,totalOrder,totalRevenue,chartData} = statData || {};
   if(isLoading) return <LoadingSpinner></LoadingSpinner>
 
   return (
@@ -94,6 +95,7 @@ const AdminStatistics = () => {
           {/*Sales Bar Chart */}
           <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2'>
             {/* Chart goes here.. */}
+            {chartData && <Chart chartData={chartData}></Chart>}
           </div>
           {/* Calender */}
           <div className=' relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden'>
---
+++ client/src/pages/Dashboard/Charts/Chart.jsx
@@ -0,0 +1,36 @@
+import { Legend } from "@headlessui/react";
+import React from "react";
+import {
+    Area,
+    Bar,
+    CartesianGrid,
+    ComposedChart,
+    Line,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from "recharts";
+
+const Chart = ({ chartData }) => {
+    console.log("chart Data ",chartData);
+
+    return (
+        <ComposedChart width={730} height={250} data={[chartData]}>
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Area
+                type="monotone"
+                dataKey="order"
+                fill="#8884d8"
+                stroke="#8884d8"
+            />
+            <Bar dataKey="price" barSize={20} fill="#413ea0" />
+            <Line type="monotone" dataKey="quantity" stroke="#ff7300" />
+        </ComposedChart>
+    );
+};
+
+export default Chart;
